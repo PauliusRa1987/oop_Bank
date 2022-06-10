090e3ee7ec122dba584e0e6b8c307d6d5462eab5
PauliusRa <buvizz@gmail.com>
2022-06-10 21:16:21 +0300

    Friday job

---
+++ src/views/bottom.php
@@ -0,0 +1,6 @@
+</main>
+<footer class="footer">
+    <p>&copy; Paulius bank project. OOP attempt</p>
+    </footer>
+</body>
+</html>
---
+++ src/views/home.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__.'/top.php';
+?>
+<main class="main">
+<h1>Paulius BANK.<br> Make Coding Simple!</h1>
+
+<?php
+require __DIR__.'/bottom.php';
+?>
